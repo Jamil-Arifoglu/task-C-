05d9faa0c7c0b0a2b0220c37a9fe9f7318a3e2a6
Jamil-Arifoglu <jamilai@code.edu.az>
2022-11-20 02:40:57 +0400

    TaskC#

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿#region Student
+//using ConsoleApp3;
+
+
+//Student_class Student = new Student_class("Cemil", "Isayev", "P230", 95, false);
+
+
+#endregion
+Console.WriteLine("cumle qeyd edin");
+string cumle=Console.ReadLine();
+string cumle2 = cumle.ToUpper();
+Console.WriteLine("cavab:");
+Ceka(cumle2);
+
+static void Ceka(string cumle)
+{
+	for (int i = 0; i < cumle.Length; i++)
+	{
+		if (i == 0)
+		{
+			Console.Write(cumle[i]);
+			cumle = cumle.ToLower();
+			i++;
+		}
+		else if (cumle[i] ==' ')
+		{    cumle=cumle.ToUpper();	
+			Console.Write(cumle[i]);
+			i++;
+		}
+		
+		Console.Write(cumle[i]);
+        cumle = cumle.ToLower();
+    }
+
+}
---
+++ ConsoleApp3/Student class.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Student_class
+    {
+        public string Name;
+        public string LastName;
+        public string Grup;
+        public byte Point;
+        public bool IsGraduated;
+
+        public Student_class(string name, string nastName, string grup, byte point, bool isGraduated)
+        {
+            Name = name;
+
+            LastName = nastName;
+            Grup = grup;
+            Point = point;
+            IsGraduated = isGraduated;
+            Console.WriteLine(name);
+            Console.WriteLine(nastName);
+            Console.WriteLine(grup);
+            Console.WriteLine(point);
+            Console.WriteLine(isGraduated);
+            Graduated();
+            Point1();
+
+        }
+
+        public void Graduated()
+        {
+            if (IsGraduated)
+            {
+                Console.WriteLine("Mezun olub");
+
+            }
+            else
+            {
+                Console.WriteLine("mezun olmayib");
+
+            }
+        }
+        public void Point1()
+        {
+            if (Point > 80)
+            {
+                Console.WriteLine("Novbeti merheleye sans qazandiniz");
+
+            }
+            else
+            {
+                Console.WriteLine("Novebeti merheleye sans qazanmadiz");
+            }
+        }
+
+
+
+
+
+
+    }
+    }
